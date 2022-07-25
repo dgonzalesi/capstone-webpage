926fd5d6a03077cd1dabc93258711fef141be836
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-07-25 15:48:08 -0500

    Initial commit, files adding, fonts, css(no - JS)

---
+++ js/test.mjs
@@ -0,0 +1,11 @@
+const data = [
+  {
+    superhero: 'Wick',
+    name: 'Jhon',
+    description: 'Testing text',
+    profile: 'URL',
+    image: 'URL',
+    date: '01/01/1970'
+  }
+]
+export default data
