8f507ca8f4c1d0053ffa07f724f9ee46a325f17b
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-07-27 18:34:24 -0500

    test error fix in about.js

--- js/about.js
+++ js/about.js
@@ -1,11 +1,11 @@
-const hamburger = document.getElementById('hamburguerlogo')
-const elementscontainer = document.getElementById('elements-container')
+const hamburger = document.getElementById('hamburguerlogo');
+const elementscontainer = document.getElementById('elements-container');
 
 hamburger.addEventListener('click', () => {
-  document.querySelector('#hamburguerlogo').classList.toggle('nodisplay')
-  document.getElementById('elements-container').classList.remove('nodisplay')
-})
+  document.querySelector('#hamburguerlogo').classList.toggle('nodisplay');
+  document.getElementById('elements-container').classList.remove('nodisplay');
+});
 elementscontainer.addEventListener('click', () => {
-  elementscontainer.classList.toggle('nodisplay')
-  document.querySelector('#hamburguerlogo').classList.toggle('nodisplay')
-})
+  elementscontainer.classList.toggle('nodisplay');
+  document.querySelector('#hamburguerlogo').classList.toggle('nodisplay');
+});
